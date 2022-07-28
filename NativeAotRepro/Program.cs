6bdbe80df1ad6464862d6af16411c5db7602976d
Gerardo Grignoli <gerardog@gmail.com>
2022-07-28 04:39:01 -0300

    Create repro for NativeAOT issue

---
+++ NativeAotRepro/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO.Pipes;
+
+Console.WriteLine("Hello, World!");
+
+var ps = new PipeSecurity();
+
+using (var dataPipe = NamedPipeServerStreamConstructors.New("test", PipeDirection.InOut, 10,
+    PipeTransmissionMode.Message, PipeOptions.Asynchronous, 1024, 1024, ps))
+{
+    Console.WriteLine("Named pipe created");
+} 
